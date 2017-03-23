cb8a2da186d12b76e0bbedab2e4048779d81f819
agariodziki <klegowski@onet.eu>
2017-03-23 16:56:25 +0100

    Kacper

--- src/agario/Main.java
+++ src/agario/Main.java
@@ -1,10 +1,11 @@
 package agario;
 
-import java.awt.EventQueue;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("hhehe");
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
