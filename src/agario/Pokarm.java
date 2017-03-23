25115caca6ac414544585a9f8769e1ea7528a720
Karoll0 <123>
2017-03-23 16:32:39 +0100

    ZIomble

--- src/agario/Pokarm.java
+++ src/agario/Pokarm.java
@@ -3,20 +3,17 @@ package agario;
 /**
  * Created by Karoll0 on 2017-03-23.
  */
-public class Gracz {
+public class Pokarm {
     private int wspol_x;
     private int wspol_y;
     private int promien;
-    private int id_gracza;
     private boolean isAlive;
-    private String name;
-    public Gracz(int x , int y, int r)
+    public Pokarm(int x , int y, int r)
     {
         this.wspol_x = x;
         this.wspol_y = y;
         this.promien = r;
     }
-
     public int getPromien() {
         return promien;
     }
@@ -39,18 +36,5 @@ public class Gracz {
 
     public void setWspol_x(int wspol_x) {
         this.wspol_x = wspol_x;
-    }
-
-    public int getId_gracza() {
-        return id_gracza;
-    }
-
-    public void setId_gracza(int id_gracza) {
-        this.id_gracza = id_gracza;
-    }
-
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
