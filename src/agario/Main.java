182b56ac5c8685b93ae2ad1beab2580ca9e64f71
agariodziki <klegowski@onet.eu>
2017-03-23 15:21:33 +0100

    siema ziomki

--- src/agario/Main.java
+++ src/agario/Main.java
@@ -5,9 +5,6 @@ public class Main {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-        System.out.println("Hello, World");
-        System.out.println("Hello, Karol");
-        System.out.println("Hello, Kacper");
     }
 
 }
