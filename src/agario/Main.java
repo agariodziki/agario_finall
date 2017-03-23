67cc912ffea04883252e92f092406a74430c5ded
Karoll0 <123>
2017-03-23 15:11:11 +0100

    COMIT COMIT

--- src/agario/Main.java
+++ src/agario/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        System.out.println("Hello, World");
     }
 
 }
