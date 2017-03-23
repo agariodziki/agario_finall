561b6253b4cce538802adc3c24dfae334c63ee83
Gucio <gustaw@ohler.pl>
2017-03-23 15:50:02 +0100

    gucio

--- src/agario/Main.java
+++ src/agario/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("GUCIO KURWA");
     }
 }
