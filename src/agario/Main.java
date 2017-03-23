6483ed0401f915f3fa3eb926c3ad4ab275e02b6a
agariodziki <klegowski@onet.eu>
2017-03-23 15:16:01 +0100

    siema ziomki

--- src/agario/Main.java
+++ src/agario/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Hello, World");
         System.out.println("Hello, World");
         System.out.println("Hello, Karol");
+        System.out.println("Hello, Kacper");
     }
 
 }
